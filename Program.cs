a7f0b1f1018662d6c23cb9b828ffd76d0847dd94
edsonarturodimasn@hotmail.com <edsonarturodimasn@hotmail.com>
2022-08-02 10:07:05 -0500

    creacion de manipulacion de procesos

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace principal
+{
+    interface humanod
+    {
+        string apellido();
+    }
+    class humano
+    {
+        private int piernas = 4;
+        public int getpiernas()
+        {
+            return piernas;
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+namespace principal
+{
+    // See https://aka.ms/new-console-template for more information
+
+    class persona : humano,humanod
+    {
+        private string nombre;
+        public persona(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string imprimirNombre()
+        {
+            return nombre;
+        }
+        public string apellido(){
+            return "dimas";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+       
+            
+
+         
+            /*startInfo.FileName = "ls";
+            startInfo.Arguments = "-a";
+            Process.Start(startInfo);*/
+            Process obj = ejecutarGoogle();
+            Console.ReadLine();
+            Console.WriteLine(obj.MachineName);
+        }
+        static Process ejecutarGoogle(){
+            string url = "\"https://www.google.com/search?q=ejecutar+chrome+en+linux&oq=ejecutar+chrome+en+l&aqs=chrome.0.0i512j69i57j0i22i30l3.7977j1j7&sourceid=chrome&ie=UTF-8\" --new-window";
+            Process newProcess = new Process();
+
+            newProcess.StartInfo.Verb = "print";
+            newProcess.StartInfo.FileName = "google-chrome";
+            newProcess.StartInfo.Arguments = url;
+            newProcess.StartInfo.CreateNoWindow = false;
+
+            newProcess.Start();
+            return newProcess;
+        } 
+
+    }
+}
